ac8dae48b35a3a505098640196602319aaece2ab
shinyay <shinya.com@gmail.com>
2020-10-13 23:12:05 +0900

    Add call repository from service

--- src/main/kotlin/com/google/shinyay/service/SimpleService.kt
+++ src/main/kotlin/com/google/shinyay/service/SimpleService.kt
@@ -1,12 +1,13 @@
 package com.google.shinyay.service
 
 import com.google.shinyay.logger
+import com.google.shinyay.repository.SimpleRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 
 @Service
-class SimpleService {
+class SimpleService(val repository: SimpleRepository) {
 
     fun justService() {
         logger.info("Start: Service")
@@ -16,6 +17,7 @@ class SimpleService {
                 delay(1000)
                 logger.info("$it:${Thread.currentThread().name}: Service - done")
             }
+            repository.justRepository()
         }
         logger.info("End: Service")
     }
