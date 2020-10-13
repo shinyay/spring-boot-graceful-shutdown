d6d21a427eba260decc122785df77e2f9a81e02c
shinyay <shinya.com@gmail.com>
2020-10-13 23:07:34 +0900

    Add call service from controller

--- src/main/kotlin/com/google/shinyay/controller/SimpleController.kt
+++ src/main/kotlin/com/google/shinyay/controller/SimpleController.kt
@@ -1,23 +1,25 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.logger
+import com.google.shinyay.service.SimpleService
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SimpleController {
+class SimpleController(val service: SimpleService) {
 
     @GetMapping("/")
     fun justController() {
         logger.info("Start: Controller")
         runBlocking {
             (1..5).forEach {
-                logger.info("$it:${Thread.currentThread().name}: delaying")
+                logger.info("$it:${Thread.currentThread().name}: Controller - delaying")
                 delay(1000)
-                logger.info("$it:${Thread.currentThread().name}: done")
+                logger.info("$it:${Thread.currentThread().name}: Controller - done")
             }
+            service.justService()
         }
         logger.info("End: Controller")
     }
