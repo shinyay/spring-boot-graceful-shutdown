56272b19fb4a592509e7a1ea6380b1e9aab20e89
shinyay <shinya.com@gmail.com>
2020-10-13 22:58:18 +0900

    Add delay function with coroutines

--- src/main/kotlin/com/google/shinyay/controller/SimpleController.kt
+++ src/main/kotlin/com/google/shinyay/controller/SimpleController.kt
@@ -1,6 +1,8 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.logger
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,6 +12,13 @@ class SimpleController {
     @GetMapping("/")
     fun justController() {
         logger.info("Start: Controller")
+        runBlocking {
+            (1..5).forEach {
+                logger.info("$it:${Thread.currentThread().name}: delaying")
+                delay(1000)
+                logger.info("$it:${Thread.currentThread().name}: done")
+            }
+        }
         logger.info("End: Controller")
     }
 }
